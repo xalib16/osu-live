53fd2fe10ea3539f4aed023474a420b2c1797beb
xalib16 <xalib16@gmail.com>
2025-03-10 02:20:09 -0400

    Delete server/src/schema/WebAPIScoresResponse.ts

--- server/src/schema/WebAPIScoresResponse.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-import { WebAPIScore } from "./WebAPIScore.js";
-
-export const WebAPIScoresResponse = z.object({
-  scores: z.array(WebAPIScore),
-  cursor: z.object({ id: z.coerce.number() }),
-  cursor_string: z.string(),
-});
