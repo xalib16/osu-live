2b19b7db0246308f0f5c931e6f67ebf3973f3ec3
xalib16 <xalib16@gmail.com>
2025-03-10 02:19:50 -0400

    Delete server/src/schema/WebAPIBeatmapSet.ts

--- server/src/schema/WebAPIBeatmapSet.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const WebAPIBeatmapSet = z.object({
-  artist: z.string(),
-  id: z.number(),
-  status: z.string(),
-  title: z.string(),
-  user_id: z.number(),
-});
