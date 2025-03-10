43867ff6523f83e36ccbf9e8bf0124972f137def
xalib16 <xalib16@gmail.com>
2025-03-10 16:08:19 -0400

    Update 0001_lonely_speed_demon.sql

--- server/src/database/migrations/postgresql/0001_lonely_speed_demon.sql
+++ server/src/database/migrations/postgresql/0001_lonely_speed_demon.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "beatmap_set" ADD CONSTRAINT "beatmap_set_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "beatmap_set" ADD CONSTRAINT "beatmap_set_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
